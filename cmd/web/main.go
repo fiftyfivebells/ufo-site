4756974ca12f95948835ef79c677cc5e72946889
Stephen Bell <stephen.l.bell@gmail.com>
2020-12-07 17:00:01 -0500

    Create info log and error log for different message types

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,15 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":3000", "HTTP network address")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -16,7 +20,7 @@ func main() {
 	mux.HandleFunc("/stats", showStatistics)
 	mux.HandleFunc("/sightings", showSightings)
 
-	log.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }

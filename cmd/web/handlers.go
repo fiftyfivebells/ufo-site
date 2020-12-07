e175efd6a7f81edf882fd71c8a9fcf90c5a0c63d
Stephen Bell <stephen.l.bell@gmail.com>
2020-12-07 17:28:04 -0500

    Update home route to use error helpers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 )
 
 // Route handler for the home page
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -21,15 +20,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal server error", 500)
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal server error", 500)
+		app.serverError(w, err)
 	}
 }
 

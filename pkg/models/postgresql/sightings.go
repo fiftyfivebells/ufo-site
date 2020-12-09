113da5015ee41a37c8045f83956f02ef39fcc3ab
Stephen Bell <stephen.l.bell@gmail.com>
2020-12-09 17:31:40 -0500

    Fill in implementation of the Insert method

--- pkg/models/postgresql/sightings.go
+++ pkg/models/postgresql/sightings.go
@@ -11,8 +11,22 @@ type SightingModel struct {
 	DB *sql.DB
 }
 
-func (m *SightingModel) Insert(userID int, datetime time.Time, season, city, state, country, shape string, duration int, latitude, longitude float64) (int, error) {
-	return 0, nil
+func (m *SightingModel) Insert(userID int, datetime time.Time, season, city, state, country, shape string, duration int, lat, long float64) (int, error) {
+	stmt := `INSERT INTO sightings (user_id, datetime, season, city, state, country, shape, duration, latitude, longitude) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+
+	result, err := m.DB.Exec(stmt, userID, datetime, season, city, state, country, shape, duration, lat, long)
+
+	if err != nil {
+		return -1, err
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return -1, err
+	}
+
+	return int(id), nil
 }
 
 func (m *SightingModel) Get(id int) (*models.Sighting, error) {

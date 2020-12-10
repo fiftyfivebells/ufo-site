2e9c6e4f0602ef702760381d7fa3b6a47fe11f75
Stephen Bell <stephen.l.bell@gmail.com>
2020-12-09 22:08:26 -0500

    Reformat for readability

--- pkg/models/postgresql/sightings.go
+++ pkg/models/postgresql/sightings.go
@@ -13,7 +13,7 @@ type SightingModel struct {
 }
 
 func (m *SightingModel) Insert(userID int, datetime time.Time, season, city, state, country, shape string, duration int, lat, long float64) (int, error) {
-	// stmt := `INSERT INTO sightings  VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+
 	query := `INSERT INTO sightings (user_id, datetime, season, city, state, country, shape, duration, latitude, longitude) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING index`
 
 	stmt, err := m.DB.Prepare(query)
@@ -21,10 +21,20 @@ func (m *SightingModel) Insert(userID int, datetime time.Time, season, city, sta
 	if err != nil {
 		return -1, err
 	}
+
 	defer stmt.Close()
 
 	var id int
-	err = stmt.QueryRow(userID, datetime, season, city, state, country, shape, duration, lat, long).Scan(&id)
+	err = stmt.QueryRow(userID,
+		datetime,
+		season,
+		city,
+		state,
+		country,
+		shape,
+		duration,
+		lat,
+		long).Scan(&id)
 
 	if err != nil {
 		return -1, err

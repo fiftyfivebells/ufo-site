2ebe012cde136111bb0a8ad2ffcf61a965716344
Stephen Bell <stephen.l.bell@gmail.com>
2020-12-10 12:56:23 -0500

    Create function to initialize a cache of the html templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,45 @@
 package main
 
-import "stephenbell.dev/ufo-site/pkg/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"stephenbell.dev/ufo-site/pkg/models"
+)
 
 type templateData struct {
 	Sighting  *models.Sighting
 	Sightings []*models.Sighting
+}
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }

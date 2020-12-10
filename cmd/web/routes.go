6721489d605110132c6cd4be79f31e76c77ab1a8
Stephen Bell <stephen.l.bell@gmail.com>
2020-12-10 14:50:11 -0500

    Add pat package and update routers to use RESTful routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,22 +3,24 @@ package main
 import (
 	"net/http"
 
+	"github.com/bmizerany/pat"
 	"github.com/justinas/alice"
 )
 
 func (app *application) Routes() http.Handler {
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequests, secureHeaders)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/report", app.reportSighting)
-	mux.HandleFunc("/stats", app.showStatistics)
-	mux.HandleFunc("/sighting", app.showSighting)
-	mux.HandleFunc("/sightings", app.showSightings)
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(app.home))
+	mux.Get("/report", http.HandlerFunc(app.reportSightingForm))
+	mux.Post("/report", http.HandlerFunc(app.reportSighting))
+	mux.Get("/stats", http.HandlerFunc(app.showStatistics))
+	mux.Get("/sighting/:id", http.HandlerFunc(app.showSighting))
+	mux.Get("/sightings/:state", http.HandlerFunc(app.showSightings))
 
 	// file server to serve static files
 	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 
 	return standardMiddleware.Then(mux)
 }

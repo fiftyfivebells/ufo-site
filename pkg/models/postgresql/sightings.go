ed01c1dbac9e468a47cf4d04c740da90921338b9
Stephen Bell <stephen.l.bell@gmail.com>
2020-12-11 22:26:13 -0500

    Delete old code after refactor

--- pkg/models/postgresql/sightings.go
+++ pkg/models/postgresql/sightings.go
@@ -34,34 +34,6 @@ func (m *SightingModel) Insert(userID int, datetime time.Time, season, city, sta
 	}
 
 	return id, nil
-
-	// query := `INSERT INTO sightings (user_id, datetime, season, city, state, country, shape, duration, latitude, longitude) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING index`
-
-	// stmt, err := m.DB.Prepare(query)
-
-	// if err != nil {
-	// 	return -1, err
-	// }
-
-	// defer stmt.Close()
-
-	// var id int
-	// err = stmt.QueryRow(userID,
-	// 	datetime,
-	// 	season,
-	// 	city,
-	// 	state,
-	// 	country,
-	// 	shape,
-	// 	duration,
-	// 	lat,
-	// 	long).Scan(&id)
-
-	// if err != nil {
-	// 	return -1, err
-	// }
-
-	// return id, nil
 }
 
 func (m *SightingModel) Get(id int) (*models.Sighting, error) {

e02ed4f76a52a8317f49e4848f42a713b3c7a187
Stephen Bell <stephen.l.bell@gmail.com>
2020-12-13 19:31:28 -0500

    Implement the Insert method for users

--- pkg/models/postgresql/users.go
+++ pkg/models/postgresql/users.go
@@ -2,7 +2,11 @@ package postgresql
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 
+	"github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 	"stephenbell.dev/ufo-site/pkg/models"
 )
 
@@ -10,8 +14,27 @@ type UserModel struct {
 	DB *sql.DB
 }
 
-func (m *UserModel) Insert(name, email, pass string) {
-	return
+func (m *UserModel) Insert(name, email, pass string) error {
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(pass), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (username, email, hashed_pass, created)
+VALUES($1, $2, $3, NOW())`
+
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPass))
+	if err != nil {
+		var pgSQLError *pq.Error
+		if errors.As(err, &pgSQLError) {
+			if strings.Contains(pgSQLError.Message, "users_uc_email") {
+				return models.ErrDuplicateEmail
+			}
+		}
+		return err
+	}
+
+	return nil
 }
 
 func (m *UserModel) Authenticate(email, pass string) (int, error) {

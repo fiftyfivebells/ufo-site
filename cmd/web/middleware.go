9cb2173a938b20467603071182f045ea9241e03a
Stephen Bell <stephen.l.bell@gmail.com>
2020-12-13 20:23:51 -0500

    Create authenticate middleware to give a context id if user is legit

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/justinas/nosurf"
+	"stephenbell.dev/ufo-site/pkg/models"
 )
 
 // Sets the some security headers for the route
@@ -52,6 +55,32 @@ func (app *application) requireAuthorization(next http.Handler) http.Handler {
 
 		w.Header().Add("Cache-Control", "no-store")
 		next.ServeHTTP(w, r)
+	})
+}
+
+// Checks to make sure the user is authenticated, and if so, add the authenticated
+// context to the request
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		exists := app.session.Exists(r, "authenticatedUserID")
+		if !exists {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		user, err := app.users.Get(app.session.GetInt(r, "authenticatedUserID"))
+		if errors.Is(err, models.ErrNoRecord) || !user.Active {
+			app.session.Remove(r, "authenticatedUserID")
+			next.ServeHTTP(w, r)
+			return
+		} else if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		// If the user is authentic and active, add the authenticated context to their request
+		ctx := context.WithValue(r.Context(), contextKeyIsAuthenticated, true)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
 

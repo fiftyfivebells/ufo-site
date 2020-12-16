5e8cccd7e280b78d3dfc088861b1eb8984aecd87
Stephen Bell <stephen.l.bell@gmail.com>
2020-12-15 21:30:20 -0500

    Create a javascript file add a query selector event

---
+++ ui/static/js/main.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log("loaded");
+  // Add event listeners to the radio buttons on the create sighting form. If the
+  // radio is No, hide UFO specific details. Otherwise, show them.
+  if (document.querySelector("input[name='sighting']")) {
+    let ufo = document.getElementById("ufoDetails");
+
+    document.querySelectorAll("input[name='sighting']").forEach((e) => {
+      if (e.value === "No" && e.checked) {
+        ufo.style.display = "none";
+      }
+
+      if (e.value === "Yes" && e.checked) {
+        ufo.style.display = "block";
+      }
+      e.addEventListener("change", function(event) {
+        let item = event.target.value;
+        if (item === "Yes") {
+          ufo.style.display = "block";
+        } else if (item === "No") {
+          ufo.style.display = "none";
+        }
+      });
+    });
+  }
+});

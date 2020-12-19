824ea4520cead81d60c252bae3908e35afe52d79
Stephen Bell <stephen.l.bell@gmail.com>
2020-12-19 12:58:31 -0500

    Get items from DB and display them in the list

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,5 +1,17 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
+  // Add event listeners to the selection bar on the sightings page
+  if (document.getElementById("sightingState")) {
+    let stateSelector = document.getElementById("sightingState");
+
+    getAllSightings();
+
+    stateSelector.addEventListener("change", function(event) {
+      let state = stateSelector.value;
+      displayListOfSightings(state);
+    });
+  }
+
   // Add event listeners to the radio buttons on the create sighting form. If the
   // radio is No, hide UFO specific details. Otherwise, show them.
   if (document.querySelector("input[name='sighting']")) {
@@ -25,6 +37,71 @@ window.addEventListener('DOMContentLoaded', (event) => {
   }
 });
 
+const sightings = []
+
+// Get JSON from database and return the Promise of the items
+async function getJson() {
+  const data = await fetch("/sightings/all");
+  const json = await data.json();
+  return json;
+}
+
+async function getAllSightings() {
+  getJson().then(d => {
+    for (let x of d) {
+      sightings.push(x);
+    }
+  });
+}
+
+function displayListOfSightings(state) {
+  let displayData;
+
+  if (state !== "") {
+    const abbrev = getKeyByValue(states, state);
+    displayData = sightings.filter(o => o.state === abbrev);
+  } else {
+    displayData = sightings.slice();
+  }
+
+  const table = document.getElementById("sightingTable");
+  table.removeChild(table.getElementsByTagName("tbody")[0]);
+  const tbody = document.createElement("tbody");
+
+  
+  for (let s of displayData) {
+    const row = tbody.insertRow();
+
+    const index = row.insertCell();
+    const link = document.createElement("a");
+    link.setAttribute("href", "/sighting/" + s.index);
+    link.innerHTML = s.index;
+    index.appendChild(link);
+
+    const date = row.insertCell();
+    date.innerHTML = s.datetime;
+
+    const city = row.insertCell();
+    city.innerHTML = s.city;
+
+    const state = row.insertCell();
+    state.innerHTML = states[s.state];
+
+    const shape = row.insertCell();
+    shape.innerHTML = s.shape.Valid ? s.shape.String : "";
+
+    const duration = row.insertCell();
+    duration.innerHTML = s.duration.Valid ? s.duration.Int64 : "";
+  }
+
+  table.appendChild(tbody);
+}
+
+// Takes in an object and value, then returns the key for that value
+// in the object, if it exists
+function getKeyByValue(obj, value) {
+  return Object.keys(obj).find(key => obj[key] === value);
+}
 
 const states = {
   "al": "Alabama",
